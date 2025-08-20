d89ece77df823ddf74d34cf17119262db46598f9
nazbur05 <nazburgenbay01@gmail.com>
2025-08-20 14:38:29 +0500

    fix issues with models, add subcategories

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Category, Customer, Products, Order
+from .models import Category, SubCategory,Product, Customer, Order
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+    list_filter = ('parent',)
+    search_fields = ('name',)
+
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('name',)
+#     search_fields = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Product)
 admin.site.register(Customer)
-admin.site.register(Products)
 admin.site.register(Order)
--- shop/models.py
+++ shop/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-import datetime
 
 User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     @staticmethod
     def get_all_categories():
@@ -14,9 +17,58 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+class SubCategory(models.Model):
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+
+    class Meta:
+        verbose_name = 'SubCategory'
+        verbose_name_plural = 'SubCategories'
+
+    @staticmethod
+    def get_all_subcategories():
+        return SubCategory.objects.all()
+
+    def __str__(self):
+        return f"{self.name} in {self.parent.name}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.IntegerField(default=0)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, default=1)
+    description = models.CharField(
+        max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/')
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def get_products_by_id(ids):
+        return Product.objects.filter(id__in=ids)
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+    
+    @staticmethod
+    def get_all_products_by_subcategoryid(subcategory_id):
+        if subcategory_id:
+            return Product.objects.filter(subcategory=subcategory_id)
+        return Product.get_all_products()
+
+    @staticmethod
+    def get_all_products_by_categoryid(category_id):
+        if category_id:
+            return Product.objects.filter(subcategory__category=category_id)
+        else:
+            return Product.get_all_products()
+        
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     phone = models.CharField(max_length=10)
+
+    favourites = models.ManyToManyField(Product, related_name='favourited_by', blank=True)
 
     def __str__(self):
         return self.user.username if self.user else "No user"
@@ -31,46 +83,18 @@ class Customer(models.Model):
             return self.user.email
         return ""
 
-class Products(models.Model):
-    name = models.CharField(max_length=60)
-    price = models.IntegerField(default=0)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(
-        max_length=250, default='', blank=True, null=True)
-    image = models.ImageField(upload_to='uploads/products/')
-
-    @staticmethod
-    def get_products_by_id(ids):
-        return Products.objects.filter(id__in=ids)
-
-    @staticmethod
-    def get_all_products():
-        return Products.objects.all()
-
-    @staticmethod
-    def get_all_products_by_categoryid(category_id):
-        if category_id:
-            return Products.objects.filter(category=category_id)
-        else:
-            return Products.get_all_products()
-
 class Order(models.Model):
-    product = models.ForeignKey(Products, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    # customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
     quantity = models.IntegerField(default=1)
     price = models.IntegerField()
     address = models.CharField(max_length=50, default='', blank=True)
     phone = models.CharField(max_length=50, default='', blank=True)
-    date = models.DateField(default=datetime.datetime.today)
+    date = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False)
 
     def placeOrder(self):
         self.save()
-
-    @staticmethod
-    def get_orders_by_user(user_id):
-        return Order.objects.filter(user=user_id).order_by('-date')
 
     @staticmethod
     def get_orders_by_customer(customer_id):
