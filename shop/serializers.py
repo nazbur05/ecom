51365beefe4bd4ffadce5eb2efd464404646ed9a
nazbur05 <nazburgenbay01@gmail.com>
2025-08-26 13:30:06 +0500

    Update serializers

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,27 +1,39 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Category, SubCategory, Product, Customer, Order
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
 
 class SubCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategory
-        fields = '__all__'
+        fields = ['id', 'name', 'parent']
+
+class CategorySerializer(serializers.ModelSerializer):
+    subcategories = SubCategorySerializer(many=True, read_only=True)
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'subcategories']
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    subcategory = SubCategorySerializer(read_only=True)
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'name', 'price', 'description', 'image', 'category', 'subcategory']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 class CustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    favourites = ProductSerializer(many=True, read_only=True)
+
     class Meta:
         model = Customer
-        fields = '__all__'
+        fields = ['id', 'user', 'phone', 'favourites']
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['id', 'product', 'customer', 'quantity', 'price', 'date', 'status']
