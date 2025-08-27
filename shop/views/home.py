7cb7db18ab1239115d577839458e9f5779780054
Naz <171716584+nazbur05@users.noreply.github.com>
2025-08-27 09:02:49 +0500

    Implement product actions for favourites and cart, update templates and context processors

--- shop/views/home.py
+++ shop/views/home.py
@@ -1,9 +1,12 @@
 # from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from shop.models import Category, Product, SubCategory
+from shop.models import Customer, Category, Product, SubCategory, Order
 
 def home_view(request):
+    customer = Customer.objects.get(user=request.user)
+    favourites = set(customer.get_favourites().values_list('id', flat=True))
+    cart_products = set(Order.objects.filter(customer=customer, status=False).values_list('product_id', flat=True))
     categories = Category.get_all_categories()
     subcategories = SubCategory.get_all_subcategories()
     products = Product.objects.all().order_by('-id')[:8]
-    return render(request, 'shop/home.html', {'categories': categories, 'subcategories': subcategories, 'products': products})
+    return render(request, 'shop/home.html', {'categories': categories, 'subcategories': subcategories, 'products': products, 'favourites': favourites, 'cart_products': cart_products})
--- shop/views/product.py
+++ shop/views/product.py
@@ -7,17 +7,21 @@ def product_api_detail(request, pk):
 def products_by_category(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
     products = Product.get_all_products_by_categoryid(category_id)
+    categories = Category.get_all_categories()
     return render(request, 'shop/products.html', {
         'products': products,
         'category': category,
         'subcategory': None,
+        'categories': categories
     })
 
 def products_by_subcategory(request, subcategory_id):
     subcategory = get_object_or_404(SubCategory, pk=subcategory_id)
     products = Product.get_all_products_by_subcategoryid(subcategory_id)
+    categories = Category.get_all_categories()
     return render(request, 'shop/products.html', {
         'products': products,
         'category': subcategory.parent,
         'subcategory': subcategory,
+        'categories': categories
     })
